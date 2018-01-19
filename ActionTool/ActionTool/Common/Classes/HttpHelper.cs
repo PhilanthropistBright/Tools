5d3ea7e78f8fad89f2dcdd8d3403944533064d3a
PhilanthropistBright <no1francis@163.com>
2018-01-19 09:13:48 +0800

    20180119 commit the action tool just a example

---
+++ ActionTool/ActionTool/Common/Classes/HttpHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionTool.Common.Classes
+{
+    public class HttpHelper
+    {
+        /// <summary>
+        /// 使用json格式进行post提交
+        /// </summary>
+        /// <param name="jsonData"></param>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string PostWithJson(string jsonData, string url, string type = "POST")
+        {
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                var data = Encoding.UTF8.GetBytes(jsonData);
+
+                request.Method = type;
+                request.ContentType = "text/json";
+                request.ContentLength = data.Length;
+
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+
+                var response = (HttpWebResponse)request.GetResponse();
+
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                return responseString;
+            }
+            catch (Exception ex)
+            {
+                return "error";
+            }
+        }
+    }
+}
---
+++ ActionTool/ActionTool/UI/Normal/Windows/Normal_Windows_Main.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ActionTool.Common.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ActionTool.UI.Normal.Windows
+{
+    /// <summary>
+    /// Normal_Windows_Main.xaml 的交互逻辑
+    /// </summary>
+    public partial class Normal_Windows_Main : Window
+    {
+        public Normal_Windows_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Commit_Click(object sender, RoutedEventArgs e)
+        {
+            if(combobox_ActionType.Text!="GET")
+            {
+                textbox_ResponseData.Text = HttpHelper.PostWithJson(textbox_Data.Text, textbox_ActionUrl.Text, combobox_ActionType.Text);
+            }
+        }
+    }
+}
